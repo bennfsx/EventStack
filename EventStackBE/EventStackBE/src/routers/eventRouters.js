194a595f4f0576d28966e43edebdd32dbe944672
bennfsx <bennfsx@gmail.com>
2024-05-02 05:04:58 +0800

    update routers

--- EventStackBE/EventStackBE/src/routers/eventRouters.js
+++ EventStackBE/EventStackBE/src/routers/eventRouters.js
@@ -22,7 +22,7 @@ const {
 router.put("/createevent", createEvent);
 router.post("/getallevent", getAllEvent);
 router.delete("/deleteevent/:eventId", deleteEventById);
-router.patch("/updateevent/:eventId", updateEventById);
+router.patch("/updatebyid/:eventId", updateEventById);
 
 router.post("/upload-image/:eventid", upload.single("image"), uploadAsset);
 
